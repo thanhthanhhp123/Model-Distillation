279e7e48b0ee60b7142e46227558770455ed65a4
thanhthanhhp123 <thanhmaxdz2003@gmail.com>
2024-04-19 20:30:29 +0700

    update

--- UNet.py
+++ UNet.py
@@ -74,7 +74,7 @@ class Down(nn.Module):
     def __init__(self, in_channels, out_channels, emb_dim=28):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2, padding=1),
+            nn.MaxPool2d(2),
             DoubleConv(in_channels, in_channels, residual=True),
             DoubleConv(in_channels, out_channels),
         )
